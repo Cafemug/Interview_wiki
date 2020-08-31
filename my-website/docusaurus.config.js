aa6d829c1b23fd2a435b687a6d19057c46e36cf2
Jeon Ga Bin <hyanghope@naver.com>
2020-08-31 21:17:21 +0900

    blog 제거

---
+++ my-website/docusaurus.config.js
@@ -0,0 +1,106 @@
+module.exports = {
+  title: 'Tech Interview Wiki',
+  tagline: 'Tech Interview Wiki',
+  url: 'http://localhost:3000/',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  favicon: 'img/circle.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'Tech Interview Wiki', // Usually your repo name.
+  plugins: [
+    "docusaurus-plugin-google-adsense",
+  ],
+  themeConfig: {
+    googleAdsense: {
+      dataAdClient: "ca-pub-9859374823916972",
+    },
+    navbar: {
+      title: 'Tech Interview Wiki with CoCo',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/rect.ico',
+      },
+      items: [
+        {
+          to: 'docs/',
+          activeBasePath: 'docs',
+          label: 'Docs',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/Cafemug',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: '전공 지식',
+              to: 'docs/',
+            },
+            {
+              label: '기술 인터뷰',
+              to: 'docs/interview',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: '1일 1커밋 오픈채팅방',
+              href: 'https://open.kakao.com/o/g99LzEgb',
+            },
+            {
+              label: 'Ainze',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              href: 'https://hyanghope.tistory.com/',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://hyanghope.tistory.com/',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
